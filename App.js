87a19a9bb7f8e6cf41fd0fb32df1a504aec8a287
Andre Pereira <andreporteira@gmail.com>
2022-11-30 14:35:53 -0300

    Images

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  //console.log("App executed");
+  //const handlePress = () => console.log("Text pressed");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
+      {/* <Image source={require("./assets/favicon.png")} /> */}
+      <Image 
+      // blurRadius={10}
+      fadeDuration={10000}
+      source={ {
+        width: 200,
+        height:300,
+        uri: 'https://picsum.photos/200/300'} } />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#c5c5c5',
     alignItems: 'center',
     justifyContent: 'center',
   },
